136546ff32e9f4c38810109d2384f357f41f8a40
Priyanka Jaiswal <priyanka.b.jaiswal@capgemini.com>
2023-10-12 15:18:50 +0530

    attachment picker package

---
+++ Sources/PJAttachmentPicker/HelperClass.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Priyanka Jaiswal on 11/10/23.
+//
+
+import Foundation
+
+
+
+//MARK: Constant String
+struct KString {
+    static let strAppHeader              = "Attachment !!"
+    static let ok                        = "OK"
+    static let cancel                    = "Cancel"
+}
---
+++ Tests/PJAttachmentPickerTests/PJAttachmentPickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PJAttachmentPicker
+
+final class PJAttachmentPickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PJAttachmentPicker().text, "Hello, World!")
+    }
+}
